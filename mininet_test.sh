a5a8d9355d3b5f77cba85a6b4b6a77cea2f3bdb9
adit negi <aditnegi1@gmail.com>
2023-02-11 15:56:06 -0600

    modify test to fit broker

--- mininet_test.sh
+++ mininet_test.sh
@@ -8,3 +8,4 @@ h7 python3 SubscriberAppln.py -d "10.0.0.1:5555" -T 4 -n sub1 > sub1.out 2>&1 &
 h8 python3 SubscriberAppln.py -d "10.0.0.1:5555" -T 5 -n sub2 > sub2.out 2>&1 &
 h9 python3 SubscriberAppln.py -d "10.0.0.1:5555" -T 4 -n sub3 > sub3.out 2>&1 &
 h10 python3 SubscriberAppln.py -d "10.0.0.1:5555" -T 5 -n sub4 > sub4.out 2>&1 &
+h11 python3 BrokerAppln.py -d "10.0.0.1:5555" -a "10.0.0.11"  > bro1.out 2>&1 &
